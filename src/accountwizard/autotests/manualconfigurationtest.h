25093182459a97628e302191bc68672c058d5180
Laurent Montel <montel@kde.org>
2023-06-02 13:46:23 +0200

    Add autotests

---
+++ src/accountwizard/autotests/manualconfigurationtest.h
@@ -0,0 +1,16 @@
+// SPDX-FileCopyrightText: 2023 Laurent Montel <montel@kde.org>
+// SPDX-License-Identifier: LGPL-2.0-or-later
+#pragma once
+
+#include <QObject>
+
+class ManualConfigurationTest : public QObject
+{
+    Q_OBJECT
+public:
+    explicit ManualConfigurationTest(QObject *parent = nullptr);
+    ~ManualConfigurationTest() override = default;
+
+private Q_SLOTS:
+    void shouldHaveDefaultValues();
+};
