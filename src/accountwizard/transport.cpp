be17ea020eb6f032042b167573a5cee02a98c364
Laurent Montel <montel@kde.org>
2023-06-07 07:26:23 +0200

    Prepare to add transport

--- src/accountwizard/transport.cpp
+++ src/accountwizard/transport.cpp
@@ -18,6 +18,32 @@ Transport::~Transport() = default;
 
 void Transport::createTransport()
 {
+    Q_EMIT info(i18n("Setting up mail transport account..."));
+    MailTransport::Transport *mt = MailTransport::TransportManager::self()->createTransport();
+    /*
+        mt->setName(m_name);
+        mt->setHost(m_host);
+        if (m_port > 0) {
+            mt->setPort(m_port);
+        }
+        if (!m_user.isEmpty()) {
+            mt->setUserName(m_user);
+            mt->setRequiresAuthentication(true);
+        }
+        if (!m_password.isEmpty()) {
+            mt->setStorePassword(true);
+            mt->setPassword(m_password);
+        }
+        mt->setEncryption(m_encr);
+        mt->setAuthenticationType(m_auth);
+        m_transportId = mt->id();
+        */
+    mt->save();
+    // TODO Q_EMIT info(i18n("Mail transport uses '%1' encryption and '%2' authentication.", m_encrStr, m_authStr));
+    MailTransport::TransportManager::self()->addTransport(mt);
+    MailTransport::TransportManager::self()->setDefaultTransport(mt->id());
+    Q_EMIT finished(i18n("Mail transport account set up."));
+
     // TODO
 }
 
