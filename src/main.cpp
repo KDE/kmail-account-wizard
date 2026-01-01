cea587c0cdb65cdff5769adcd2c8895a7c863b96
Laurent Montel <montel@kde.org>
2026-01-01 11:40:41 +0100

    GIT_SILENT: happy new year

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 // SPDX-FileCopyrightText: 2009 Volker Krause <vkrause@kde.org>
 // SPDX-FileCopyrightText: 2023 Carl Schwan <carl@carlschwan.eu>
-// SPDX-FileCopyrightText: 2023-2025 Laurent Montel <montel@kde.org>
+// SPDX-FileCopyrightText: 2023-2026 Laurent Montel <montel@kde.org>
 // SPDX-License-Identifier: LGPL-2.0-or-later
 
 #include <Akonadi/Control>
